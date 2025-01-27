45e359b0153fe57c767d1a9faf85469e9718faf4
maxdarwin <72539638+1darshanpatil@users.noreply.github.com>
2025-01-27 07:50:28 -0800

    Create eulerpent_eng.py

---
+++ eulerpent_eng.py
@@ -0,0 +1,19 @@
+# Yet to implement as of epoch @1737992903
+"""
+Citation:
+  #1 :: https://www.youtube.com/watch?v=iJ8pnCO0nTY
+  #2 :: https://oeis.org/A001318
+  #3 :: https://en.wikipedia.org/wiki/Pentagonal_number_theorem
+  #4 :: https://pages.uoregon.edu/koch/PentagonalNumbers.pdf
+  
+  NOTE: The video lecture by Dr. Burkard Polster on his YouTube channel, Mathologer, was instrumental in inspiring this algorithm. 
+  The implementation below is intended to demonstrate what he referred to as the "Euler-chain-engine," rather than simply solving 
+  the problem using Pentagonal Numbers or related concepts.
+  
+  I would also like to emphasize that, while Dr. Burkard's explanations and animations make the algorithm easy to understand, 
+  coders are encouraged to pay close attention to the memory consumption of their machines when implementing this solution.
+
+Thanks,  
+Darshan P.
+Commit@1737993021
+"""
