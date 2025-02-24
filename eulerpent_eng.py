9a0355e7d390161bf3e98de2cdbd4a970c66eafa
maxdarwin <72539638+1darshanpatil@users.noreply.github.com>
2025-02-24 16:47:12 +0530

    Update eulerpent_eng.py

--- eulerpent_eng.py
+++ eulerpent_eng.py
@@ -17,3 +17,44 @@ Thanks,
 Darshan P.
 Commit@1737993021
 """
+
+
+# Human:
+# Partition of N(say100) then you have to set BREAK_POINT=101
+
+# Computers:
+# Partition of N(say100) then you have to find the value of 100th Index of ColVect
+
+# len(ColVect) = BREAK_POINT
+# Therefore, BREAK_POINT = N+1 ( Where: N = Partition you are trying to find )
+
+N=666
+# SET THE VALUE OF PARTITION YOU WANT TO FIND
+BREAK_POINT = N+1
+def mult(r, c):
+    return sum(r[i]*c[i] for i in range(len(r)))
+colvect = [1]
+rowvect = []
+n, p = 1, 1
+k = 1
+pent = lambda x: x * ( 3 * x - 1 ) // 2
+while True:
+    pos_neg = 0
+    if n == pent(k*p):
+        if p % 2 == 0:
+            pos_neg = -1
+        else:
+            pos_neg = 1
+        if k == 1 :
+            k = -1
+        else:
+            k = 1
+            p += 1
+    rowvect.insert(0, pos_neg)
+    n += 1
+    colvect.append(mult(rowvect, colvect))
+    if n == BREAK_POINT:
+        break
+#print("colVect", colvect)
+print(colvect[N])
+
